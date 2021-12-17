337a9220b6ed76e54830dcee923396f74be62ab0
“liujunzhang” <liujunzhang007@sina.cn>
2021-12-17 16:08:53 +0800

    add file

---
+++ LPAccountInfoViewController.h
@@ -0,0 +1,15 @@
+//
+//  LPAccountInfoViewController.h
+//  LesPark
+//
+//  Created by he on 16/7/12.
+//
+//
+
+#import "SourceViewController.h"
+
+@interface LPAccountInfoViewController : SourceViewController
+//用户信息
+@property (nonatomic, strong) NSMutableDictionary *userData;
+
+@end
